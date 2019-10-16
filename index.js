e8872b3e13476e88c4ffe558146aab6242f99d15
Benn Lazell <benn@redrabbits.co.uk>
2019-10-16 12:27:56 +0100

    Multiple outputs

--- index.js
+++ index.js
@@ -19,7 +19,16 @@ const exec = require('@actions/exec');
     await exec.exec("GitVersion", [], options);
     
     const data = JSON.parse(gitversion);
+
+    core.setOutput('major', data.Major);
+    core.setOutput('minor', data.Minor);
+    core.setOutput('patch', data.Patch);
+    core.setOutput('build', data.BuildMetaData);
+    core.setOutput('semver', data.SemVer);
     core.setOutput('fullsemver', data.FullSemVer);
+    core.setOutput('assemblysemver', data.AssemblySemVer);
+    core.setOutput('gitversion', gitversion);
+
     console.log(`GitVersion: ${gitversion}`);
   } catch (error) {
     core.setFailed(error.message);
