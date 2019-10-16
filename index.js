eff9d4bcc0f6fe69c904f4c4b65c4586f6a8f9bf
Benn Lazell <benn@redrabbits.co.uk>
2019-10-16 12:04:41 +0100

    Async tings

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-try {
-  // `args` input defined in action metadata file
-  const args = core.getInput('args');
-  console.log(`Running GitVersion with args: ${args}`);
-  
-  var gitversion = "";
-  const options = {};
-  options.listeners = {
-    stdout: (data) => {
-      var output = data.toString();
-      gitversion += output;
-    }
-  };
+await getVersion();
 
-  exec.exec("GitVersion", [args], options);
-  
-  core.setOutput('fullsemver', gitversion);
-  console.log(`GitVersion: ${gitversion}`);
-} catch (error) {
-  core.setFailed(error.message);
+async function getVersion() {
+  try {
+    // `args` input defined in action metadata file
+    const args = core.getInput('args');
+    console.log(`Running GitVersion with args: ${args}`);
+    
+    var gitversion = "";
+    const options = {};
+    options.listeners = {
+      stdout: (data) => {
+        var output = data.toString();
+        gitversion += output;
+      }
+    };
+
+    await exec.exec("GitVersion", [args], options);
+    
+    core.setOutput('fullsemver', gitversion);
+    console.log(`GitVersion: ${gitversion}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
